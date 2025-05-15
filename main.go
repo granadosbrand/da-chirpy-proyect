a32f88daec72fedf14b6457c96bb93b1e0f8f853
Diego Granados <diego.granados@sonepar.co>
2025-05-14 22:44:07 -0500

    CH2-L1

--- main.go
+++ main.go
@@ -1,16 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	FileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.FileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metrics(w http.ResponseWriter, r *http.Request) {
+	log.Print("Accediendo a las métricas")
+	hits := cfg.FileServerHits.Load()
+	w.Write(fmt.Appendf(nil, "Hits: %d", hits))
+}
+func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	cfg.FileServerHits.Swap(0)
+	w.Write([]byte("Counter reset successfully"))
+}
 
 func main() {
 	const port = "8080"
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	apiCfg := &apiConfig{}
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.metrics)
+	mux.HandleFunc("/reset", apiCfg.reset)
 
 	server := &http.Server{
 		Handler: mux,
@@ -23,7 +50,7 @@ func main() {
 }
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type:", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
 }
